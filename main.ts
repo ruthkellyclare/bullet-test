99f8690c9cb1e57588b919148289d07fc1f3f96e
ruthkellyclare <84312230+ruthkellyclare@users.noreply.github.com>
2021-10-16 17:03:48 +0100

    Speed up when level increases

--- main.ts
+++ main.ts
@@ -33,7 +33,7 @@ sprites.onOverlap(SpriteKind.good_guy_bullet, SpriteKind.Enemy, function (sprite
         value.destroy()
     }
     game.splash("On to the next Level!!")
-    badguy.setVelocity(20 * level, 0)
+    badguy.setVelocity(30 * level, 0)
 })
 sprites.onOverlap(SpriteKind.bad_guy_bullet, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeLifeBy(-1)
@@ -47,45 +47,45 @@ let level = 0
 let badguy: Sprite = null
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . 3 3 . . . . . . . 
-    . . . . . 3 3 3 3 3 . . . . . . 
-    . . . . 3 3 3 3 3 3 3 . . . . . 
-    . . . 3 3 3 3 3 3 3 3 . . . . . 
-    . . . 3 3 3 3 3 3 3 3 3 . . . . 
-    . . 3 3 3 3 3 3 3 3 3 3 . . . . 
-    . . 3 3 3 3 3 3 3 3 3 3 . . . . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 . . 
-    . . 3 3 3 3 3 3 3 3 3 3 3 3 3 . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . 7 7 . . . . . . . 
+    . . . . . . . f f . . . . . . . 
+    . . . . . . a e e a . . . . . . 
+    . . . . a e e e e e a . . . . . 
+    . . . a e e e e e e e . . . . . 
+    . . . e e e e e e e e a . . . . 
+    . . a e e e e e e e e e . . . . 
+    . . e e e e e e e e e e a . . . 
+    . . e e e e e e e e e e f f f . 
+    . . e e e e e e e e e e e 9 . . 
+    . . e e e e e e e e e e e e e . 
+    . . e . . . . . . . . . e . . . 
     `, SpriteKind.Player)
 info.setScore(0)
 controller.moveSprite(mySprite)
 mySprite.setPosition(80, 100)
 badguy = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . 2 . . . . 2 . . . . . 2 . . . 
-    . . 2 . . 2 2 . . . 2 2 . . . 2 
-    . . 2 2 . 2 . 2 2 2 2 . . . 2 2 
-    . . . 2 2 2 2 . . . 2 2 2 2 2 . 
-    . . 2 2 . . . . . . . . 2 2 . . 
-    . . 2 . . . . . . . . . . 2 . . 
-    . . 2 . . . 2 . . . 2 . . 2 . . 
-    . . 2 . . . . . . . . . . 2 . . 
-    . . 2 . . . . . . . . . . 2 . . 
-    . . 2 . . 2 2 2 2 . . . . . 2 . 
-    . . 2 . 2 2 . . 2 2 . . . . 2 . 
-    . . 2 . . . . . . 2 2 . . 2 2 . 
-    . . 2 . . . . . . . . . . 2 . . 
-    . . 2 . . . . . . . . . . 2 . . 
-    . . 2 2 2 2 2 2 2 2 2 2 2 2 . . 
+    ......244444442.....
+    .....24444444442....
+    ....2a242444242a2...
+    .15.2a644444446a2.51
+    .2222aa6444446aa2222
+    .1522aa6644466aa2251
+    ...22aaa33333aaa22..
+    ....2aaa33333aaa2...
+    ....2aaa33333aaa2...
+    .....2aa33333aa2....
+    ......2aa333aa2.....
+    ......422222224.....
+    ......4.......4.....
+    ......4.......4.....
+    ......4.......4.....
+    ....444.......44....
     `, SpriteKind.Enemy)
-badguy.setVelocity(20, 0)
+badguy.setVelocity(100, 0)
 badguy.setPosition(20, 20)
 badguy.setBounceOnWall(true)
 level = 1
